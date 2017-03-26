0ea5abaea191593e2e5bd150ea02155438410732
xf <xiefahit@gmail.com>
2017-03-26 09:24:23 +0800

    add rest files

---
+++ db/migrate/20170326011035_create_topics.rb
@@ -0,0 +1,10 @@
+class CreateTopics < ActiveRecord::Migration[5.0]
+  def change
+    create_table :topics do |t|
+      t.string :title
+      t.text :descrption
+
+      t.timestamps
+    end
+  end
+end
